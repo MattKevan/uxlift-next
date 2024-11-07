58cbdc8a44b06c4d6aa2c84d795694d3f9ef5c8b
Matt Kevan <m@kevan.tv>
2024-11-07 15:10:07 +0000

    Update news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,6 +10,7 @@ import {
   PaginationPage,
   PaginationGap,
 } from '@/components/catalyst/pagination'
+import { PostHorizontal } from '@/components/posts/PostsHorizontal'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -75,17 +76,25 @@ export default async function NewsPage({
   const visiblePages = getVisiblePages()
 
   return (
-    <div className="max-w-4xl mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-8">Latest News</h1>
+    <main>
 
-      <div className="space-y-8">
-        {posts?.map((post) => (
-          <PostItemSmall key={post.id} post={post} />
-        ))}
+    <div className='px-6 mb-24 sm:mb-32 mt-6'>
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 md:w-3/4 lg:w-4/5 tracking-tight">Latest news,  <span className="text-gray-500">updated daily.</span></h1>
       </div>
+      <h2  className="text-lg font-bold pl-6 pt-4 bg-white/70 backdrop-blur-lg sticky top-[58px] pb-4 border-b z-40">{count} UX articles</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 border-b'>
+          {posts?.map((post) => (
+            <div className='border-b md:odd:border-r p-6 sm:[&:nth-last-child(-n+2)]:border-b-0 [&:nth-last-child(-n+1)]:border-b-0'>
+
+              <PostHorizontal key={post.id} post={post} />
+            </div>
+          ))}
+        </div>
+    
+    
 
       {totalPages > 1 && (
-        <Pagination className="mt-8">
+        <Pagination className="p-4 border-b">
           <PaginationPrevious 
             href={currentPage > 1 ? `/news?page=${currentPage - 1}` : null} 
           />
@@ -111,6 +120,6 @@ export default async function NewsPage({
           />
         </Pagination>
       )}
-    </div>
+    </main>
   )
 }
